12cbfee36b663811743eb6f137f83a482c1645bf
abdulmohammed007 <59744519+abdulmohammed007@users.noreply.github.com>
2022-01-23 11:28:00 -0700

    updated exchange deploywih connected aps

--- scripts/exchange-deploy.sh
+++ scripts/exchange-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export MAVEN_HOME=/Applications/apache-maven-3.6.3
 export PATH=$PATH:$MAVEN_HOME/bin
-mvn clean package deploy  -Dusername=abdul_shnew -Dpassword=Admin@1234 -DapplicationName=sh-mule-api -Denvironment=Dev  -Dprovider=MC -Dtarget=sh-dev-rtf -Dlogging.level.logger-mule-extension=INFO -Danypoint.platform.client_id=4df945eec34b41c9a11ec1a23a59ab83 -Danypoint.platform.client_secret=58CD558CB60b4a09b21089616eFD4E7F -Dmule.env=dev
+mvn clean package deploy  -DclientId=2490d64b638545a3861623eb3dcf20da -DclientSecret=71884803334B4C6297E6456a795Bc47B -DapplicationName=sh-mule-api -Denvironment=Dev  -Dprovider=MC -Dtarget=sh-dev-rtf -Dlogging.level.logger-mule-extension=INFO -Danypoint.platform.client_id=4df945eec34b41c9a11ec1a23a59ab83 -Danypoint.platform.client_secret=58CD558CB60b4a09b21089616eFD4E7F -Dmule.env=dev
